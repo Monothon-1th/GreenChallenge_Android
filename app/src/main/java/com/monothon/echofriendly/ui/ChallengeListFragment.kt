b429fea2ee67218eea038600b5fa025585465f75
foreknowledge <kyj_0129@naver.com>
2022-01-09 00:13:54 +0900

    리스트 보여주기

--- app/src/main/java/com/monothon/echofriendly/ui/ChallengeListFragment.kt
+++ app/src/main/java/com/monothon/echofriendly/ui/ChallengeListFragment.kt
@@ -1,11 +1,12 @@
 package com.monothon.echofriendly.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.monothon.echofriendly.R
 import com.monothon.echofriendly.databinding.FragmentChallengeListBinding
 import com.monothon.echofriendly.ui.adapter.ChallengeListAdapter
@@ -18,7 +19,7 @@ class ChallengeListFragment(private val viewModel: EchoViewModel) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_challenge_list, container, false)
         return binding.root
@@ -33,13 +34,18 @@ class ChallengeListFragment(private val viewModel: EchoViewModel) : Fragment() {
         subscribeUI()
     }
 
-    private fun subscribeUI() {
-        viewModel.challengeList.observe(requireActivity()) { listAdapter.resetList(it) }
-    }
-
     private fun configureUI() {
+        binding.lifecycleOwner = this
+        binding.challengeList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            binding.challengeList.adapter = listAdapter
+        }
         listAdapter.onItemClick = {
             // TODO
         }
+    }
+
+    private fun subscribeUI() {
+        viewModel.challengeList.observe(requireActivity()) { listAdapter.resetList(it) }
     }
 }
